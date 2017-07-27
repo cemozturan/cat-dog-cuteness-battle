0c385c92ea04650415fbaaea2c835b2a737b8bbf
Cem Ozturan <cozturan@findmypast.com>
2017-07-27 11:47:13 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var HomePage = function() {
+  return (
+    <h1>HELLO</h1>
+  );
+}
+
+ReactDOM.render(
+  <HomePage />,
+  document.getElementById('root'));
