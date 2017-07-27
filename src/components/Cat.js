a288bf0f7cc90b543d6f4bb985de2382ddfb3f4c
Cem Ozturan <cozturan@findmypast.com>
2017-07-27 14:08:50 +0100

    Adding buttons and click handlers + inline styling

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-var Cat = function() {
-  return (
-    <div style={compStyle}>
-      <h3>Cat component</h3>
-      <img
-        src="http://www.cutestpaw.com/wp-content/uploads/2011/11/HALP.jpeg"
-        alt="Cute Cat"
-        style={{height: 400, width: 400}}/>
-    </div>
-  );
+class Cat extends Component {
+  handleLikeButtonClick() {
+    console.log('Cat liked');
+  }
+
+  handleDislikeButtonClick() {
+    console.log('Cat disliked');
+  }
+
+  render() {
+    return (
+      <div style={compStyle}>
+        <h3>Cat component</h3>
+        <img
+          src="http://www.cutestpaw.com/wp-content/uploads/2011/11/HALP.jpeg"
+          alt="Cute Cat"
+          style={{height: 400, width: 400}}/>
+        <br />
+        <button style={buttonStyle} onClick={this.handleLikeButtonClick}>Like</button>
+        <button style={buttonStyle} onClick={this.handleDislikeButtonClick}>Dislike</button>
+      </div>
+    );
+  }
 }
+
+var buttonStyle = {
+  height: '25px',
+  width: '70px',
+  marginTop: '10px',
+  marginLeft: '5px',
+  marginRight: '5px'
+};
 
 var compStyle = {
   display: 'inline-block',
