b7dff5cb3775a2987747b7dfec4fad9101f159fa
Cem Ozturan <cozturan@findmypast.com>
2017-07-28 13:04:10 +0100

    Lifting the state (like/dislike count) up from Pet to HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Pet from './Pet';
 
 var style = {
@@ -7,21 +7,117 @@ var style = {
   color: 'rebeccapurple'
 };
 
-var HomePage = function() {
-  return (
-    <div>
-      <h1 style={style}>Welcome to Cat vs Dog Cuteness Battle</h1>
-      <div style={{marginTop: 60, textAlign: 'center'}}>
-        <Pet
-          imageUrl="http://www.cutestpaw.com/wp-content/uploads/2011/11/HALP.jpeg"
-          name="Cat" />
-        {' '}
-        <Pet
-          imageUrl="https://s-media-cache-ak0.pinimg.com/736x/82/b5/42/82b542ced27fcefc0935373e7c7db113--dachshund-puppies-wiener-dogs.jpg"
-          name="Dog" />
+var buttonStyle = {
+  height: '25px',
+  marginTop: '30px',
+  marginRight: '5px'
+};
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      catLikes: 0,
+      dogLikes: 0
+    }
+    /*
+      Unless you use the ES6 arrow functions, this is the best practice to make sure the
+      "this" keyword inside the class functions refer to the component instance.
+      When we do the binding here in the constructor, they get bound only once.
+      If we bind inside the onClick HTML attribute, then the binding will happen again and again
+      every time the component is rendered.
+    */
+    this.handleLikeButtonClick = this.handleLikeButtonClick.bind(this);
+    this.handleDislikeButtonClick = this.handleDislikeButtonClick.bind(this);
+    this.handleShowWinnerButtonClick = this.handleShowWinnerButtonClick.bind(this);
+    this.handleStartOverButtonClick = this.handleStartOverButtonClick.bind(this);
+  }
+
+  handleLikeButtonClick(event) {
+    var petName = event.target.value;
+
+    if (petName === 'Cat') {
+      this.setState(function(prevState) {
+        return {
+          catLikes: prevState.catLikes + 1,
+          dogLikes: prevState.dogLikes
+        }
+      });
+    } else {
+      this.setState(function(prevState) {
+        return {
+          catLikes: prevState.catLikes,
+          dogLikes: prevState.dogLikes + 1
+        }
+      });
+    }
+  }
+
+  handleDislikeButtonClick(event) {
+    var petName = event.target.value;
+
+    if (petName === 'Cat') {
+      this.setState(function(prevState) {
+        return {
+          catLikes: prevState.catLikes - 1,
+          dogLikes: prevState.dogLikes
+        }
+      });
+    } else {
+      this.setState(function(prevState) {
+        return {
+          catLikes: prevState.catLikes,
+          dogLikes: prevState.dogLikes - 1
+        }
+      });
+    }
+  }
+
+  handleShowWinnerButtonClick() {
+    var catLikesCount = this.state.catLikes;
+    var dogLikesCount = this.state.dogLikes;
+
+    if (catLikesCount > dogLikesCount) {
+      console.log('Cat is the Winner');
+    } else if (catLikesCount < dogLikesCount) {
+      console.log('Dog is the Winner');
+    } else {
+      console.log('Game is a TIE');
+    }
+  }
+
+  handleStartOverButtonClick() {
+
+  }
+
+  render () {
+    return (
+      <div>
+        <h1 style={style}>Welcome to Cat vs Dog Cuteness Battle</h1>
+        <div style={{marginTop: 60, textAlign: 'center'}}>
+          <Pet
+            imageUrl="http://www.cutestpaw.com/wp-content/uploads/2011/11/HALP.jpeg"
+            name="Cat"
+            likesCount={this.state.catLikes}
+            onLikeButtonClick={this.handleLikeButtonClick}
+            onDislikeButtonClick={this.handleDislikeButtonClick}
+          />
+          {' '}
+          <Pet
+            imageUrl="https://s-media-cache-ak0.pinimg.com/736x/82/b5/42/82b542ced27fcefc0935373e7c7db113--dachshund-puppies-wiener-dogs.jpg"
+            name="Dog"
+            likesCount={this.state.dogLikes}
+            onLikeButtonClick={this.handleLikeButtonClick}
+            onDislikeButtonClick={this.handleDislikeButtonClick}
+          />
+        </div>
+        <div style={{textAlign: 'center'}}>
+          <button style={buttonStyle} onClick={this.handleShowWinnerButtonClick}>Show Winner</button>
+          <button style={buttonStyle} onClick={this.handleStartOverButtonClick}>Start Over</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default HomePage;
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -17,39 +17,19 @@ var compStyle = {
 class Pet extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      likesCount: 0
-    };
-    /*
-      Unless you use the ES6 arrow functions, this is the best practice to make sure the
-      "this" keyword inside the class functions refer to the component instance.
-      When we do the binding here in the constructor, they get bound only once.
-      If we bind inside the onClick HTML attribute, then the binding will happen again and again
-      every time the component is rendered.
-    */
-    this.handleLikeButtonClick = this.handleLikeButtonClick.bind(this);
-    this.handleDislikeButtonClick = this.handleDislikeButtonClick.bind(this);
-  }
-
-  handleLikeButtonClick() {
-    this.setState({likesCount: this.state.likesCount+1});
-  }
-
-  handleDislikeButtonClick() {
-    this.setState({likesCount: this.state.likesCount-1});
   }
 
   render() {
     return (
       <div style={compStyle}>
-        <h3>{this.props.name} Likes: {this.state.likesCount}</h3>
+        <h3>{this.props.name} Likes: {this.props.likesCount}</h3>
         <img
           src={this.props.imageUrl}
           alt={`Cute ${this.props.name}`}
           style={{height: 400, width: 400}}/>
         <br />
-        <button style={buttonStyle} onClick={this.handleLikeButtonClick}>Like</button>
-        <button style={buttonStyle} onClick={this.handleDislikeButtonClick}>Dislike</button>
+        <button style={buttonStyle} value={this.props.name} onClick={this.props.onLikeButtonClick}>Like</button>
+        <button style={buttonStyle} value={this.props.name} onClick={this.props.onDislikeButtonClick}>Dislike</button>
         </div>
     );
   }
